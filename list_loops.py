3bb6f9714d99c95975f4053958fcdc8f34b9ae7d
Arjun Ramesh <69367261+theArj@users.noreply.github.com>
2020-09-25 12:29:29 -0700

    Q4

--- list_loops.py
+++ list_loops.py
@@ -5,3 +5,10 @@ print(songs[2])
 print(songs[1:2])
 
 songs[2] = "I've Been Flushed From the Bathroom of Your Heart"
+
+songs.append("Satan Gave Me a Taco")
+songs.extend(["You're the Reason Our Kids Are So Ugly",
+              "If You Don't Believe I Love You, Just Ask My Wife", "She Never Told Me She Was a Mime"])
+songs.insert(2, "If the Phone Doesn't Ring, It's Me")
+
+songs.remove("Do It")
